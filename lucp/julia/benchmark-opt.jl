61fb7399a32e53a205c224fe96c54316d059cb89
Jiahao Chen (陈家豪) <jiahao@mit.edu>
2016-02-23 15:53:36 -0500

    Provide completely pivoted LU benchmark.
    Joint work with @andreasnoack @jakebolewski, lightly modified to fix
    Julia 0.4 syntax.

---
+++ lucp/julia/benchmark-opt.jl
@@ -0,0 +1,13 @@
+include("lucp-opt.jl")
+timings = zeros(0, 9);
+for n = [10, 20, 50, 100, 200, 500, 1000, 2000]
+    A = randn(n, n);
+    t = zeros(5);
+    for i = 1:5
+        t[i] = @elapsed lucompletepiv3!(A);
+    end
+    newtimings = [n, t..., minimum(t), mean(t), std(t)]
+    println(newtimings)
+    timings = [timings; newtimings']
+end
+writecsv("../julia-opt.csv", timings)
